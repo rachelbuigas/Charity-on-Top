edbd68e1a5b1e6196be6979513ec4c115d8e9bec
rachelbuigas <31712935+rachelbuigas@users.noreply.github.com>
2020-07-28 16:28:58 -0400

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,7 @@
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://www.charityontop.org/assets/uploads/favicon.ico?v=2';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
